cc884720400bea1cf694d8233799e09c7d3d7c71
LAPTOP-4SUD1DQD\ramiro <ramirorf@gmail.com>
2021-12-29 00:35:49 +0100

    eliminar valores imposibles en grupo y correciones

--- src/app/sudoku1/servicio/sudoku-service.service.ts
+++ src/app/sudoku1/servicio/sudoku-service.service.ts
@@ -43,45 +43,83 @@ export class SudokuServiceService {
       for(var j=0; j!=sudoku.celdas[i].length; j++) {
         // si hay valor definido
         if (sudoku.celdas[i][j].posibles.length == 1) {
-          const valor : number = sudoku.celdas[i][j].posibles [0];
+          const valor : number = sudoku.celdas[i][j].posibles[0];
           // simplificar la fila
           {
             let celdasFila : SudokuCelda[] = this.getFila(sudoku,i);
-            this.simplificarGrupo(celdasFila,valor);
+            this.eliminarValorGrupo(celdasFila,valor);
           }
           // simplificar la columna
           {
             let celdasColumna : SudokuCelda[] = this.getColumna(sudoku,j);
-            this.simplificarGrupo(celdasColumna,valor);
+            this.eliminarValorGrupo(celdasColumna,valor);
           }
 
           // simplificar el cuadrado
           {
             let celdasCuadrado : SudokuCelda[] = this.getCuadrado(sudoku,i,j);
-            this.simplificarGrupo(celdasCuadrado,valor);
+            this.eliminarValorGrupo(celdasCuadrado,valor);
           }
+        }
+      }
+    }
+
+    for(var i=0; i!=sudoku.celdas.length; i++) {
+      for(var j=0; j!=sudoku.celdas[i].length; j++) {
+        // simplificar la fila
+        {
+          let celdasFila : SudokuCelda[] = this.getFila(sudoku,i);
+          this.simplificarGrupo(celdasFila);
+        }
+        // simplificar la columna
+        {
+          let celdasColumna : SudokuCelda[] = this.getColumna(sudoku,j);
+          this.simplificarGrupo(celdasColumna);
+        }
+
+        // simplificar el cuadrado
+        {
+          let celdasCuadrado : SudokuCelda[] = this.getCuadrado(sudoku,i,j);
+          this.simplificarGrupo(celdasCuadrado);
         }
       }
     }
   }
 
-  simplificarGrupo(celdas: SudokuCelda[], valor: number) {
+  eliminarValorGrupo(celdas: SudokuCelda[], valor: number) {
+    // eliminar valores posibles por estar ya fijados en una celda del grupo
     for(var i=0;i!=celdas.length;i++) {
-      if( celdas[i].posibles.length != 1) {
+      if( celdas[i].posibles.length > 1) {
        celdas[i].posibles = celdas[i].posibles.filter(item => item!=valor);
       }
     }
   }
 
+  simplificarGrupo(celdas: SudokuCelda[]) {
+    // fijar valor si sólo es posible en una celda
+    SUDOKU_ALL_VALUES.forEach(value => {      
+      let celdasValue : SudokuCelda[] = [];
+      for(var i=0;i!=celdas.length;i++) {
+        if(celdas[i].posibles.includes(value)) {
+          celdasValue.push(celdas[i]);
+        }
+      }
+      if (celdasValue.length == 1 && celdasValue[0].posibles.length > 1) {
+        celdasValue[0].posibles = [value];
+      }   
+    });
+
+  }
+
   getCuadrado(sudoku: Sudoku, fila: number, columna: number): SudokuCelda[] {
-    let fila_min = Math.floor(fila/3);
+    let fila_min = Math.floor(fila/3)*3;
     let fila_max = fila_min+2;
-    let columna_min = Math.floor(columna/3);
+    let columna_min = Math.floor(columna/3)*3;
     let columna_max = columna_min+2;
     let celdas : SudokuCelda[] = [];
     for(var i=0; i!=sudoku.celdas.length; i++) {
       for(var j=0; j!=sudoku.celdas[i].length; j++) {
-        if ( i >= fila_min && i <= fila_max && j <= columna_min && j >= columna_max ) {
+        if ( i >= fila_min && i <= fila_max && j >= columna_min && j <= columna_max ) {
           celdas.push(sudoku.celdas[i][j]);
         } 
       }
