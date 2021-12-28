c5711d3dc22946c48c0c7caaa56393747ab4b217
LAPTOP-4SUD1DQD\ramiro <ramirorf@gmail.com>
2021-12-28 01:29:18 +0100

    eliminar posibles de filas, columnas y grupos

--- src/app/sudoku1/servicio/sudoku-service.service.ts
+++ src/app/sudoku1/servicio/sudoku-service.service.ts
@@ -33,10 +33,75 @@ export class SudokuServiceService {
 
   siguiente(sudoku: Sudoku): Sudoku {
     let sudokuNext : Sudoku = this.copiarSudoku(sudoku);
+    this.simplificar(sudokuNext);
     return sudokuNext;
   }
 
-  copiarSudoku(sudokuSrc: Sudoku): Sudoku {
+  simplificar(sudoku: Sudoku): void {
+    // eliminar posibles  de filas, columnas y cuadrados
+    for(var i=0; i!=sudoku.celdas.length; i++) {
+      for(var j=0; j!=sudoku.celdas[i].length; j++) {
+        // si hay valor definido
+        if (sudoku.celdas[i][j].posibles.length == 1) {
+          const valor : number = sudoku.celdas[i][j].posibles [0];
+          // simplificar la fila
+          {
+            let celdasFila : SudokuCelda[] = this.getFila(sudoku,i);
+            this.simplificarGrupo(celdasFila,valor);
+          }
+          // simplificar la columna
+          {
+            let celdasColumna : SudokuCelda[] = this.getColumna(sudoku,j);
+            this.simplificarGrupo(celdasColumna,valor);
+          }
+
+          // simplificar el cuadrado
+          {
+            let celdasCuadrado : SudokuCelda[] = this.getCuadrado(sudoku,i,j);
+            this.simplificarGrupo(celdasCuadrado,valor);
+          }
+        }
+      }
+    }
+  }
+
+  simplificarGrupo(celdas: SudokuCelda[], valor: number) {
+    for(var i=0;i!=celdas.length;i++) {
+      if( celdas[i].posibles.length != 1) {
+       celdas[i].posibles = celdas[i].posibles.filter(item => item!=valor);
+      }
+    }
+  }
+
+  getCuadrado(sudoku: Sudoku, fila: number, columna: number): SudokuCelda[] {
+    let fila_min = Math.floor(fila/3);
+    let fila_max = fila_min+2;
+    let columna_min = Math.floor(columna/3);
+    let columna_max = columna_min+2;
+    let celdas : SudokuCelda[] = [];
+    for(var i=0; i!=sudoku.celdas.length; i++) {
+      for(var j=0; j!=sudoku.celdas[i].length; j++) {
+        if ( i >= fila_min && i <= fila_max && j <= columna_min && j >= columna_max ) {
+          celdas.push(sudoku.celdas[i][j]);
+        } 
+      }
+    }
+    return celdas;
+  }
+
+  getColumna(sudoku: Sudoku, columna: number): SudokuCelda[] {
+    let celdas : SudokuCelda[] = [];
+    for(var i=0;i!=sudoku.celdas.length;i++) {
+      celdas.push(sudoku.celdas[i][columna]);
+    }
+    return celdas;
+  }
+
+  getFila(sudoku: Sudoku, i: number): SudokuCelda[] {
+    return sudoku.celdas[i];
+  }
+
+  private copiarSudoku(sudokuSrc: Sudoku): Sudoku {
 
     // copiar el array de celdas
     let celdas: SudokuCelda[][] = [];
@@ -53,7 +118,7 @@ export class SudokuServiceService {
     return sudoku;
   }
 
-  copiarCelda(sudokuCeldaSrc: SudokuCelda): SudokuCelda {
+  private copiarCelda(sudokuCeldaSrc: SudokuCelda): SudokuCelda {
     let sudokuCelda : SudokuCelda = { posibles : Object.assign([], sudokuCeldaSrc.posibles) };
     return sudokuCelda;
   }
