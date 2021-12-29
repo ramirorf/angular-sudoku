93ce12f967fbdbffc3cb169a6012d40e60de9d7c
LAPTOP-4SUD1DQD\ramiro <ramirorf@gmail.com>
2021-12-30 00:04:46 +0100

    añadir menú

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Sudoku1Component } from './sudoku1/sudoku1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sudoku1', component: Sudoku1Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
