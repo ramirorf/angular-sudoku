fab753a6c34143433d78ea1891e6c013260ac5f5
LAPTOP-4SUD1DQD\ramiro <ramirorf@gmail.com>
2021-12-31 01:10:49 +0100

    incluir angular material (datepicker)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { Sudoku1Component } from './sudoku1/sudoku1.component';
 import { NasaApodComponent } from './nasa-apod/nasa-apod.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -16,8 +21,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/nasa-apod/nasa-apod.component.ts
+++ src/app/nasa-apod/nasa-apod.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { formatDate } from '@angular/common';
 
 interface APOD {
   title: string;
@@ -14,12 +16,35 @@ interface APOD {
 })
 export class NasaApodComponent implements OnInit {
 
-  apod : APOD = {url:"", title:""};
+  static URL_BASE : string ="https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
 
-  constructor(private http: HttpClient) { }
+  profileForm = new FormGroup({
+    date: new FormControl(''),
+  });
+
+  apod : APOD = {url:"", title:""};
+  date : Date = new Date();
+
+  constructor(private http: HttpClient, @Inject(LOCALE_ID) private locale: string) { }
 
   ngOnInit(): void {
-    this.http.get<APOD>("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY").subscribe((apod : APOD) => {
+    this.http.get<APOD>(NasaApodComponent.URL_BASE).subscribe((apod : APOD) => {
+      this.apod=apod;
+    });
+  }
+
+  dateChange(event: MatDatepickerInputEvent<Date>) {
+    if( event.value != null) {
+      this.consultar(event.value);
+    }
+  }
+
+  consultar(date: Date) {
+    let url: string = NasaApodComponent.URL_BASE;
+    if(date != null) {
+      url = `${url}&date=${formatDate(date, "yyyy-MM-dd", this.locale)}`;
+    }
+    this.http.get<APOD>(url).subscribe((apod : APOD) => {
       this.apod=apod;
     });
   }
