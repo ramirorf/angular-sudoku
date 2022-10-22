cb5e8eaaa7d5d3b0a5014e9d2a2bdb7dba148d92
LAPTOP-4SUD1DQD\ramiro <ramirorf@gmail.com>
2022-10-23 01:15:24 +0200

    juego vida: tablero

--- src/app/juego-vida/juego-vida.component.ts
+++ src/app/juego-vida/juego-vida.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JuegoVidaComponent implements OnInit {
 
+  TABLERO_SIZE : number = 10; 
+
+  juegoVidaTablero : boolean[][] = [];
+
+  paso : number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+      this.juegoVidaTablero = this.intTablero(this.TABLERO_SIZE);
+  }
+
+  onAnteriorPaso(): void {
+    if (this.paso > 0) {
+      this.paso--;
+    }
+  }
+
+  onSiguientePaso(): void {
+    this.paso++;
+  }
+
+  intTablero(size : number) {
+    let tablero : boolean[][] = [];
+    for(let i=0;i<size;i++){
+      let fila : boolean [] = [];
+      for(let j=0;j<size;j++) {
+        fila.push((i+j)%2 == 1);
+      } 
+      tablero.push(fila);
+    } 
+    return tablero;
   }
 
 }
