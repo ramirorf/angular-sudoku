0c0c1ffb3f85b615c80a1e1b6e9daeabfdb82471
LAPTOP-4SUD1DQD\ramiro <ramirorf@gmail.com>
2022-10-23 00:19:53 +0200

    juego-vida component vinculación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JuegoVidaComponent } from './juego-vida/juego-vida.component';
 import { NasaApodComponent } from './nasa-apod/nasa-apod.component';
 import { Sudoku1Component } from './sudoku1/sudoku1.component';
 
 const routes: Routes = [
   { path: 'sudoku1', component: Sudoku1Component },
   { path: 'nasa-apod', component: NasaApodComponent },
+  { path: 'juego-vida', component: JuegoVidaComponent },
 ];
 
 @NgModule({
