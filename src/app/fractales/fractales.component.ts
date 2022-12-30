3d2dcd2bd0ed49c8b06a81e7bc94169da116ea50
LAPTOP-4SUD1DQD\ramiro <ramirorf@gmail.com>
2022-12-30 02:31:53 +0100

    refactorización

--- src/app/fractales/fractales.component.ts
+++ src/app/fractales/fractales.component.ts
@@ -7,21 +7,43 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class FractalesComponent implements OnInit {
 
-  constructor() { }
+  height : number = 1000;
+  width : number = 1000;
+  context2D : CanvasRenderingContext2D | null = null;
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
 
   onSiguientePaso(): void {
-    // obtener el contexto de dibujo 2d del canvas
-    let canvas = document.getElementById('fractal-canvas') as HTMLCanvasElement;
-    let ctx = canvas.getContext('2d');
-    if (ctx) {
-      // dibujar línea
-      ctx.moveTo(0, 0);
-      ctx.lineTo(200, 100);
-      ctx.stroke();
+    this.drawLine();
+  }
+
+  // métodos de dibujo
+
+  drawLine() {
+    // dibujar línea
+    let ctx = this.getContext2D();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+  }
+
+  // auxiliares
+
+  private getContext2D () {
+    if ( !this.context2D ) {
+      // obtener el contexto de dibujo 2d del canvas
+      let canvas = document.getElementById('fractal-canvas') as HTMLCanvasElement;
+      this.context2D = canvas.getContext('2d');
+      if (this.context2D) {
+        this.context2D.translate(this.width/2, this.height/2);
+      } 
+      throw new Error('No canvas available');
     }
+    return this.context2D;
   }
 
 }
