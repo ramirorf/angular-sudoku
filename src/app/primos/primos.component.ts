bdc301bdae482b4e2be9eebd62b0edf5057d5ffd
LAPTOP-4SUD1DQD\ramiro <ramirorf@gmail.com>
2023-11-19 00:26:59 +0100

    factorización de números

--- src/app/primos/primos.component.ts
+++ src/app/primos/primos.component.ts
@@ -11,16 +11,73 @@ export class PrimosComponent {
 
   primosArray: number[];
   primos: string;
+  divisoresString: string = "";
 
   constructor() {
     this.primos = "";
     this.primosArray = [2];
   }
 
-  numeroChange(event: any) {
+  numeroPrimosChange(event: any) {
     if (event.target.value != null) {
       this.primos = this.calcularPrimos(event.target.value);
     }
+  }
+
+  numeroDescomponerChange(event: any) {
+    if (event.target.value != null) {
+      let numero = event.target.value;
+      let divisores = this.descomponer(numero);
+      
+      // convertir los factores en texto
+      this.divisoresString = "";
+      divisores.forEach((value: number, key: number) => {
+        if (this.divisoresString == "") {
+          this.divisoresString += key + "^" + value;
+        } else {
+          this.divisoresString += " + " +key + "^" + value;
+        }
+      });
+      this.divisoresString = " \( " + this.divisoresString + " \) ";
+      /*
+      let divisores = new Map<number, number>();
+      let currentPrimoCandidato = 0;      
+      do {
+        let primoCandidato =  obtenerSiguentePrimo(currentPrimoCandidato); 
+        numero = numero;
+        currentPrimoCandidato++; 
+      } while(numero % obtenerSiguentePrimo(currentPrimoCandidato) == 0) {
+      */
+
+      //this.primos = this.calcularPrimos(event.target.value);
+    }
+  }
+
+  descomponer ( number : number) : Map<number, number> {
+    let divisores = new Map<number, number>();
+    let currentPrimoCandidato = 0;      
+
+   do {
+    let primo = this.primosArray[currentPrimoCandidato];
+    let contador = 0;
+    while ( number != 1 && number % primo == 0) {
+      number = number / primo;
+      contador++;  
+    }
+    if (contador > 0) {
+      divisores.set(primo,contador);       
+    }
+
+    currentPrimoCandidato++;
+    this.inicializarPrimosArray(this.primosArray, currentPrimoCandidato+1);
+   } while( number != 1);
+
+   return divisores;
+   /*
+    divisores.set(2,2);
+    divisores.set(3,2);
+    return divisores;
+    */
   }
 
   calcularPrimos(number: number): string {
